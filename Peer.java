ea4cf7d2bfdeed7d12b8ec49bb04101201e39867
Kritika Prakash <krpr96@gmail.com>
2017-08-20 23:40:58 +0530

    Add files via upload

--- Peer.java
+++ Peer.java
@@ -24,13 +24,19 @@ public class Peer {
 			int sPort = Integer.parseInt(serverPort);
 			String clientPort = args[1];
 			int cPort = Integer.parseInt(clientPort);
+
 			String person = args[2];
 			String friend = args[3];
+
+			String serverPortUDP = args[4];
+			int sPortUDP = Integer.parseInt(serverPortUDP);
+			String clientPortUDP = args[5];
+			int cPortUDP = Integer.parseInt(clientPortUDP);
 			// server port is the port of this system
 			try {
 
-				Server myServer = new Server(sPort,person,friend);
-				Client myClient = new Client(cPort,person,friend);
+				Server myServer = new Server(sPort,sPortUDP,person,friend);
+				Client myClient = new Client(cPort,cPortUDP,person,friend);
 				myServer.setPriority(Thread.MAX_PRIORITY);
 				myClient.setPriority(Thread.NORM_PRIORITY);
 				myServer.start();
@@ -46,7 +52,7 @@ public class Peer {
 
 		}
 		catch(ArrayIndexOutOfBoundsException e) {
-            System.out.println("Please enter both ports and usernames on the command line");
+            System.out.println("Please enter all four ports and usernames on the command line");
         }
 	}
 }
